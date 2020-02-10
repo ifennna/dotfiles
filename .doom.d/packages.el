42185f41bde5da6642031596b1ff736c995fcf49
Ifenna Awagu <jawagu@gmail.com>
2020-02-10 13:17:37 +0100

    Add doom emacs

---
+++ .doom.d/packages.el
@@ -0,0 +1,12 @@
+;;; .doom.d/packages.el
+
+;;; Examples:
+;; (package! some-package)
+;; (package! another-package :recipe (:host github :repo "username/repo"))
+;; (package! builtin-package :disable t)
+(package! CIDER)
+(package! clojure-mode)
+(package! flycheck-clojure)
+(package! rainbow-delimiters)
+
+(provide 'packages)
